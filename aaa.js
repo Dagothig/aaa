fff6dc18f9f7ea8846e6e3d7a3e0e38932029e54
Dagothig <guillaumenoelmartel@gmail.com>
2020-05-10 11:57:01 -0400

    No-cache headers.

--- aaa.js
+++ aaa.js
@@ -8,7 +8,10 @@ const root = path.dirname(require.main.filename);
 const aaa = fs.promises.readdir('aaa');
 
 app.get('/', (req, res) =>
-    aaa.then(files =>
-        res.sendFile(`aaa/${ files.random() }`, { root })));
+    aaa.then(files => {
+        res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+        res.setHeader('Pragma', 'no-cache, no-store');
+        res.sendFile(`aaa/${ files.random() }`, { root })
+    }));
 
 server.listen(process.env.SERVER_PORT || 1337);
